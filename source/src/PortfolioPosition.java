d5dcbff52684134b024f7667aa608efd5b031de3
FilipSzzz <121720963+FilipSzzz@users.noreply.github.com>
2025-05-16 21:01:02 +0200

    Update PortfolioPosition.java

--- source/src/PortfolioPosition.java
+++ source/src/PortfolioPosition.java
@@ -1,4 +1,4 @@
-public record PortfolioPosition(Asset asset, int quantity) {
+public record PortfolioPosition(Asset asset, int quantity) { // wskazuje że każda instancja tej klasy musi mieć konkretny obiekt aktywa i liczbę sztuk. wiaze kazde aktywo z dana iloscia
     public PortfolioPosition{
         if (asset == null || quantity <= 0) {
             throw new IllegalArgumentException("Niepoprawne dane");
